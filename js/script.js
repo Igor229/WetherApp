f830d88f7e5105bb6187f7b2799e5aa1abd27318
Igor229 <frunza2029@gmail.com>
2021-02-10 16:40:19 +0200

    Stathem Wether and new Card

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
-fetch ('http://api.openweathermap.org/data/2.5/weather?id=693510&appid=742198c70f710c4c4e1999121c8c386b')
+fetch ('http://api.openweathermap.org/data/2.5/weather?id=707471&appid=742198c70f710c4c4e1999121c8c386b')
 	.then (function (resp) { return resp.json() }) //convert to json
 	.then (function (data) {
 		console.log(data);
-		document.querySelector('.package-name').textContent = data.name;
-		document.querySelector('.price').innerHTML = Math.round(data.main.temp - 273.15) + '&deg;';
+		document.querySelector('.city_name').textContent = data.name;
+		document.querySelector('.temp').innerHTML = Math.round(data.main.temp - 273.15) + '&deg;';
 
 	})
 	.catch (function() {
