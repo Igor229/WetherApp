82c9c1750e6512794be186e94005c7f01f7a02ff
Igor229 <frunza2029@gmail.com>
2021-02-14 00:40:08 +0200

    looks better js

--- js/script.js
+++ js/script.js
@@ -2,31 +2,52 @@
 	.then (function (resp) { return resp.json() }) //convert to json
 	.then (function (data) {
 		console.log(data);
-		// document.querySelector('.city__name').textContent = data.name;
-		// document.querySelector('.temp').innerHTML = Math.round(data.main.temp - 273.15) + '&deg;';
-
+		document.querySelector('.current__city').textContent = data.name;
+		document.querySelector('.current__temp').innerHTML = Math.round(data.main.temp - 273.15) + '&deg;';
+		document.querySelector('.feelslike').innerHTML = Math.round(data.main.feels_like - 273.15) + '&deg;';
+		document.querySelector('.humidity').innerHTML = data.main.humidity + "%";
+		document.querySelector('.pressure').innerHTML = Math.round(data.main.pressure / 1.33) + "мм";
+		document.querySelector('.wind').innerHTML = Math.round(data.wind.speed) + "м/с";
 	})
-	.catch (function() {
-		// catch any errors
-	});
+
+	Number.prototype.pad = function(size) {
+	  var s = String(this);
+	  while (s.length < (size || 2)) {s = "0" + s;}
+	  return s;
+	}
 
 
-	// 48.300702 25.223240
-	// 693510
+	function getHoursString(dateTime) {
+		  let date = new Date(dateTime);
+		  let hours = date.getHours().pad();
+
+		  return hours;
+		}
 
 
+	function getIcon (data) {
+		let forecastDataContainer = document.querySelector('.forecast');
+		let forecasts = ''
+
+		for (let i = 0; i < 6; i++) {
+			let item = data.list[i];
 
 
+			let icon = item.weather[0].icon;
+			let temp = getTemperature(item.main.temp -273.15);
+			let time = ( i == 0 ? 'Нині' : getHoursString(item.dt * 1000) + ":00");
+
+			let template = `<div class="forecast__item">
+			<div class="forecast__time">${time}</div>
+			<div class="forecast__icon icon__${icon}"></div>
+			<div class="forecast__temp">${temp}</div>
+			</div>`;
+			forecasts += template;
+		}
+		forecastDataContainer.innerHTML = forecasts;
+	}
 
 
-
-
-
-
-
-
-
-	// const url = 'js/data.json';
 	const temperatureUnit = '˚';
 	const humidityUnit = ' %';
 	const pressureUnit = ' мм. рт. ст.';
@@ -49,19 +70,6 @@
 	  return (value/1.33 ).toFixed();
 	}
 
-	Number.prototype.pad = function(size) {
-	  var s = String(this);
-	  while (s.length < (size || 2)) {s = "0" + s;}
-	  return s;
-	}
-
-	function getHoursString(dateTime) {
-	  let date = new Date(dateTime);
-	  let hours = date.getHours().pad();
-
-	  return hours;
-	}
-
 	function getValueWithUnit(value, unit) {
 	  return `${value}${unit}`;
 	}
@@ -72,65 +80,8 @@
 	}
 
 	function render(data) {
-	  renderCity(data);
-	  renderCurrentTemperature(data);
+	  getIcon(data);
 
-	  renderForecast(data);
-	  renderDetails(data);
-
-	}
-
-	function renderCity(data) {
-	  let cityName = document.querySelector('.current__city');
-	  cityName.innerHTML = data.city.name;
-	}
-
-	function renderCurrentTemperature(data) {
-	  let tmp = data.list[0].main.temp;
-
-	  let currentTmp = document.querySelector('.current__temp');
-	  currentTmp.innerHTML = getTemperature(tmp -273.15);
-	}
-
-
-	function renderForecast(data) {
-	  let forecastDataContainer = document.querySelector('.forecast');
-	  let forecasts = '';
-
-	  for (let i = 0; i < 6; i++) {
-	    let item = data.list[i];
-
-	    let icon = item.weather[0].icon;
-	    let temp = getTemperature(item.main.temp -273.15);
-	    let hours = ( i == 0 ? 'Нині' : getHoursString(item.dt * 1000) + ":00");
-
-	    let template = `<div class="forecast__item">
-	      <div class="forecast__time">${hours}</div>
-	      <div class="forecast__icon icon__${icon}"></div>
-	      <div class="forecast__temperature">${temp}</div>
-	    </div>`;
-	    forecasts += template;
-	  }
-	  forecastDataContainer.innerHTML = forecasts;
-	}
-
-	function renderDetails(data) {
-	  let item = data.list[0];
-	  let pressureValue = convertPressure(item.main.pressure);
-	  let pressure = getValueWithUnit(pressureValue, pressureUnit);
-	  let humidity = getValueWithUnit(item.main.humidity, humidityUnit);
-	  let feels_like = getTemperature(item.main.feels_like -273.15);
-	  let wind = getValueWithUnit(item.wind.speed, windUnit);
-
-	  renderDetailsItem('feelslike', feels_like);
-	  renderDetailsItem('humidity', humidity);
-	  renderDetailsItem('pressure', pressure);
-	  renderDetailsItem('wind', wind);
-	}
-
-	function renderDetailsItem(className, value) {
-	  let container = document.querySelector(`.${className}`).querySelector('.details__value');
-	  container.innerHTML = value;
 	}
 
 	function start() {
@@ -140,14 +91,4 @@
 	  })
 	}
 
-	function transition() {
-	  document.documentElement.classList.add('transition');
-	  setTimeout(function() {
-	    document.documentElement.classList.remove('transition');
-	  }, 4000)
-	}
-
 	start();
-
-
-
